58cb96c9283a9d4f2cde9eeabc9aa289731b77ea
ramboll <ramboll@live.com>
2022-03-07 15:55:04 +0800

    add BytesPool

--- pool/bytes_pool.go
+++ pool/bytes_pool.go
@@ -3,16 +3,36 @@ package pool
 import "sync"
 
 const (
-	DefaultMaxBytesCap = 1 << 10 // 1k
+	DefaultMaxBytesCap = 16 << 10 // 16k
 	DefaultNewBytesCap = 512
 )
 
-var gloableBytesPool *BytesPool
+var globalBytesPool *BytesPool
+var bytesPoolOnce sync.Once
 
 type BytesPool struct {
 	p       *sync.Pool
 	maxCap  int
 	initCap int
+}
+
+func NewBytesPool(initCap, maxCap int) *BytesPool {
+	if initCap < 1 {
+		initCap = DefaultNewBytesCap
+	}
+	if maxCap < initCap {
+		maxCap = initCap
+	}
+	return &BytesPool{
+		p: &sync.Pool{
+			New: func() interface{} {
+				bz := make([]byte, 0, initCap)
+				return &bz
+			},
+		},
+		maxCap:  maxCap,
+		initCap: initCap,
+	}
 }
 
 func (p *BytesPool) Get() *[]byte {
@@ -25,4 +45,24 @@ func (p *BytesPool) Put(bz *[]byte) {
 	}
 	b := (*bz)[:0]
 	p.p.Put(&b)
+}
+
+func initGlobalBytesPool() {
+	bytesPoolOnce.Do(func() {
+		globalBytesPool = NewBytesPool(DefaultNewBytesCap, DefaultMaxBytesCap)
+	})
+}
+
+func BytesPoolPut(bz *[]byte) {
+	initGlobalBytesPool()
+	globalBytesPool.Put(bz)
+}
+
+func BytesPoolGet() *[]byte {
+	initGlobalBytesPool()
+	return globalBytesPool.Get()
+}
+
+func SetBytesPoolMaxCap(maxCap int) {
+	globalBytesPool.maxCap = maxCap
 }
